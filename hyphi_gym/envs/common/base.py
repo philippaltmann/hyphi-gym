af40366d7845cf268f8a25ddc2746f6683923854
Philipp Altmann <philipp.altmann@ifi.lmu.de>
2023-05-14 16:34:27 +0200

    Fixed base typing

--- hyphi_gym/envs/common/base.py
+++ hyphi_gym/envs/common/base.py
@@ -1,18 +1,28 @@
+from typing import Any, List, SupportsFloat
 import gymnasium as gym 
 from gymnasium.envs.registration import EnvSpec
+from gymnasium.utils import seeding
+
 
 STOP_PERCENTAGE = 0.95
+TARGET_REWARD, STEP_COST, HOLE_COST = 50, -1, -50
+WALL, FIELD, AGENT, TARGET, HOLE = '#', ' ', 'A', 'T', 'H'
+CELL_LOOKUP = [WALL, FIELD, AGENT, TARGET, HOLE]
+CELL_SIZE = 64
+UP, RIGHT, DOWN, LEFT = 0, 1, 2, 3; ACTIONS = [UP, RIGHT, DOWN, LEFT]
+RAND_KEY = ['Agent', 'Target']; RAND_KEYS = ['Agents', 'Targets']; RAND = ['Layouts', *RAND_KEY, *RAND_KEYS]
 
 class Base(gym.Env):
   """ Base Env Implementing 
   • Sparse Rewards (defaults to False)
   • Early Stopping on reward threshold (defaults to False) TODO: MOVE TO WRAPPER?
   • Trucated Episodes (via `max_episode_steps`)"""
+  random: List; termination_resaons:List; _name: str
+
   def __init__(self, max_episode_steps=100, sparse=False, explore=False, seed=None): #stop=False,
     self.dynamic_spec = ['reward_threshold', 'nondeterministic', 'max_episode_steps']
-    self.reward_buffer, self._spec, self.sparse, self.explore = None, {}, sparse, explore 
-    self.max_episode_steps = max_episode_steps; self.termination_resaons = None
-    self.nondeterministic = len(self.random) > 0; self.seed(seed)
+    self._spec, self.max_episode_steps, self.sparse, self.explore = {}, max_episode_steps, sparse, explore 
+    self.nondeterministic = len(self.random) > 0; self._np_random, seed = seeding.np_random(seed)
 
   @property #if stop else None TODO: handle stopping via training param
   def reward_threshold(self): return round(.95 * self.reward_range[1])
@@ -26,16 +36,18 @@ class Base(gym.Env):
   @spec.setter
   def spec(self, spec): self._spec = {k:v for k,v in spec.__dict__.items() if k not in ['namespace','name','version']}
 
-  def reset(self, **kwargs): self.reward_buffer, self.termination_resaons = [], []; super().reset(**kwargs)
+  def reset(self, **kwargs): 
+    self.reward_buffer, self.termination_resaons = [], []; 
+    return super().reset(**kwargs)
 
-  def seed(self, seed=None): self._np_random, seed = gym.utils.seeding.np_random(seed) if seed is not None else (None, None)
+  def _step(self, action:Any) -> tuple[Any, SupportsFloat, bool, bool, dict[str, Any]]:
+    raise(NotImplementedError)
   
-  def step(self, action):
-    state, reward, terminated, truncated, info = self._step(action); 
+  def step(self, action:Any) -> tuple[Any, SupportsFloat, bool, bool, dict[str, Any]]:
+    state, reward, terminated, truncated, info = self._step(action); self.reward_buffer.append(reward)
     if self.explore: reward = 0
-    self.reward_buffer.append(reward)
     truncated = self.max_episode_steps is not None and len(self.reward_buffer) >= self.max_episode_steps
     if truncated: info = {'termination_reason': 'TIME', **info}; self.termination_resaons.append('TIME')
-    if truncated or terminated: info = {'episode': {'l': len(self.reward_buffer), 'r': sum(self.reward_buffer)}, **info}
+    # if truncated or terminated: info = {'episode': {'l': len(self.rewards), 'r': sum(self.rewards)}, 'history':self._history(), **info}
     if self.sparse: reward = 0 if not (terminated or truncated) else sum(self.reward_buffer)
     return state, reward, terminated, truncated, info
