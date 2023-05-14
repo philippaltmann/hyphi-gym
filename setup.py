4a2f2fcb583e092dc79b121925ec480d586c3a75
Philipp Altmann <philipp.altmann@ifi.lmu.de>
2023-05-14 03:44:24 +0200

    Updated setup

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ setup(
   author='Philipp Altmann',
   author_email='philipp@hyphi.co',
   url='https://gym.hyphi.co',
-  packages=find_packages(include=['hyphi_gym', 'hyphi_gym.*']),
   install_requires=[
     'gymnasium==0.28.1',
     'bpy==3.4.0', # Render 3D
