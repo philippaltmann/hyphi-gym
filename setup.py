0be8d60e48aae91cd6477a495bceb4d5cf396a73
Philipp Altmann <philipp.altmann@ifi.lmu.de>
2023-05-14 11:29:10 +0200

    Updated setup

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
-from setuptools import setup, find_packages
+from pathlib import Path
+from setuptools import find_packages, setup
 
 setup(
   name='hyphi-gym',
   version='1.0',
-  description='Python Distribution Utilities',
+  description='Gymnasium benchmark suite for evaluating robustness and multi-task performance of reinforcement learning algorithms in various discrete and continuous environments.',
   author='Philipp Altmann',
   author_email='philipp@hyphi.co',
-  url='https://gym.hyphi.co',
-  dependency_links = ['http://github.com/philippaltmann/hyphi-gym'],
+  url='https://github.com/philippaltmann/hyphi-gym',
+  license='MIT',
+  long_description=(Path(__file__).parent / "README.md").read_text(),
+  long_description_content_type="text/markdown",
+  packages=find_packages(exclude=["test"]),
   install_requires=[
     'gymnasium==0.28.1',
     'bpy==3.4.0', # Render 3D
