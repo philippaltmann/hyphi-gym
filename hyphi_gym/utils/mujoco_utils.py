0064f6373fad850442e28829b0f896223d32c88a
Philipp Altmann <philipp.altmann@ifi.lmu.de>
2023-06-24 17:07:04 +0200

    Added mujoco utilities

---
+++ hyphi_gym/utils/mujoco_utils.py
@@ -0,0 +1,115 @@
+from typing import Any, Dict, Tuple, Union
+
+try:
+  from mujoco import MjModel as MujocoModel           # type: ignore
+  from mujoco import mjtObj as MujocoObject           # type: ignore
+  from mujoco import mj_name2id as mujoco_name2id     # type: ignore
+except ImportError as e: 
+  import gymnasium
+  raise gymnasium.error.DependencyNotInstalled( f"{e}. (HINT: you need to install mujoco, run `pip install gymnasium[mujoco]`.)")
+
+MJ_OBJ_TYPES = ["mjOBJ_BODY","mjOBJ_JOINT","mjOBJ_GEOM","mjOBJ_SITE","mjOBJ_CAMERA","mjOBJ_ACTUATOR","mjOBJ_SENSOR"]
+
+def extract_mj_names(model: MujocoModel, obj_type: MujocoObject) -> Tuple[Union[Tuple[str, ...], Tuple[()]], Dict[str, int], Dict[int, Any]]:
+  if obj_type == MujocoObject.mjOBJ_BODY: name_addr = model.name_bodyadr; n_obj = model.nbody
+  elif obj_type == MujocoObject.mjOBJ_JOINT: name_addr = model.name_jntadr; n_obj = model.njnt
+  elif obj_type == MujocoObject.mjOBJ_GEOM: name_addr = model.name_geomadr; n_obj = model.ngeom
+  elif obj_type == MujocoObject.mjOBJ_SITE: name_addr = model.name_siteadr; n_obj = model.nsite
+  elif obj_type == MujocoObject.mjOBJ_LIGHT: name_addr = model.name_lightadr; n_obj = model.nlight
+  elif obj_type == MujocoObject.mjOBJ_CAMERA: name_addr = model.name_camadr; n_obj = model.ncam
+  elif obj_type == MujocoObject.mjOBJ_ACTUATOR: name_addr = model.name_actuatoradr; n_obj = model.nu
+  elif obj_type == MujocoObject.mjOBJ_SENSOR: name_addr = model.name_sensoradr; n_obj = model.nsensor
+  elif obj_type == MujocoObject.mjOBJ_TENDON: name_addr = model.name_tendonadr; n_obj = model.ntendon
+  elif obj_type == MujocoObject.mjOBJ_MESH: name_addr = model.name_meshadr; n_obj = model.nmesh
+  else: raise ValueError(f"{obj_type} was passed as the MuJoCo model object type but can only be of the following MujocoObject enum types: {MJ_OBJ_TYPES}.")
+  id2name = {i: None for i in range(n_obj)}; name2id = {}
+  for addr in name_addr:
+    name = model.names[addr:].split(b"\x00")[0].decode()
+    if name:
+      obj_id = mujoco_name2id(model, obj_type, name)
+      assert 0 <= obj_id < n_obj and id2name[obj_id] is None
+      name2id[name] = obj_id; id2name[obj_id] = name
+  return tuple(id2name[id] for id in sorted(name2id.values())), name2id, id2name
+
+class MujocoModelNames:
+  """Access MujocoObject object names and ids of the current MuJoCo model.
+  This class supports access to the names and ids of the following mjObj types:
+    mjOBJ_BODY mjOBJ_JOINT mjOBJ_GEOM mjOBJ_SITE mjOBJ_CAMERA mjOBJ_ACTUATOR mjOBJ_SENSOR
+  The properties provided for each ``mjObj`` are:
+    ``mjObj``_names: list of the mjObj names in the model of type mjOBJ_FOO.
+    ``mjObj``_name2id: dictionary with name of the mjObj as keys and id of the mjObj as values.
+    ``mjObj``_id2name: dictionary with id of the mjObj as keys and name of the mjObj as values. """
+
+  def __init__(self, model: MujocoModel):
+    """Access MujocoObject object names and ids of the current MuJoCo model.
+    Args: model: mjModel of the MuJoCo environment. """
+    (self._body_names, self._body_name2id, self._body_id2name) = extract_mj_names(model, MujocoObject.mjOBJ_BODY)
+    (self._joint_names, self._joint_name2id, self._joint_id2name) = extract_mj_names(model, MujocoObject.mjOBJ_JOINT)
+    (self._geom_names, self._geom_name2id, self._geom_id2name) = extract_mj_names(model, MujocoObject.mjOBJ_GEOM)
+    (self._site_names, self._site_name2id, self._site_id2name) = extract_mj_names(model, MujocoObject.mjOBJ_SITE)
+    (self._camera_names, self._camera_name2id, self._camera_id2name) = extract_mj_names(model, MujocoObject.mjOBJ_CAMERA)
+    (self._actuator_names, self._actuator_name2id, self._actuator_id2name) = extract_mj_names(model, MujocoObject.mjOBJ_ACTUATOR)
+    (self._sensor_names, self._sensor_name2id, self._sensor_id2name) = extract_mj_names(model, MujocoObject.mjOBJ_SENSOR)
+
+  @property
+  def body_names(self): return self._body_names
+
+  @property
+  def body_name2id(self): return self._body_name2id
+
+  @property
+  def body_id2name(self): return self._body_id2name
+
+  @property
+  def joint_names(self): return self._joint_names
+
+  @property
+  def joint_name2id(self): return self._joint_name2id
+
+  @property
+  def joint_id2name(self): return self._joint_id2name
+
+  @property
+  def geom_names(self): return self._geom_names
+
+  @property
+  def geom_name2id(self): return self._geom_name2id
+
+  @property
+  def geom_id2name(self): return self._geom_id2name
+
+  @property
+  def site_names(self): return self._site_names
+
+  @property
+  def site_name2id(self): return self._site_name2id
+
+  @property
+  def site_id2name(self): return self._site_id2name
+
+  @property
+  def camera_names(self): return self._camera_names
+
+  @property
+  def camera_name2id(self): return self._camera_name2id
+
+  @property
+  def camera_id2name(self): return self._camera_id2name
+
+  @property
+  def actuator_names(self): return self._actuator_names
+
+  @property
+  def actuator_name2id(self): return self._actuator_name2id
+
+  @property
+  def actuator_id2name(self): return self._actuator_id2name
+
+  @property
+  def sensor_names(self): return self._sensor_names
+
+  @property
+  def sensor_name2id(self): return self._sensor_name2id
+
+  @property
+  def sensor_id2name(self): return self._sensor_id2name
