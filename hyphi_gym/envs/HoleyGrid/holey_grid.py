1d76cb8fd85680e7e77c9f00acab5bfddfbd05b6
Philipp Altmann <philipp.altmann@ifi.lmu.de>
2023-07-17 17:05:49 +0200

    Refactor HoleyGrid to parent class w Point support

--- hyphi_gym/envs/HoleyGrid/holey_grid.py
+++ hyphi_gym/envs/HoleyGrid/holey_grid.py
@@ -1,23 +1,22 @@
-from hyphi_gym.envs.common.grid import *
+from hyphi_gym.envs.common import Grid, Holes
 
-LEVELS = {
-  'Train': (['#########',
-             '#A HHH T#',
-             '#       #',
-             '#       #',
-             '#       #',
-             '#  HHH  #',
-             '#########'], (-float(150), float(42))),
-  'Shift': (['#########',
-             '#A HHH T#',
-             '#  HHH  #',
-             '#       #',
-             '#       #',
-             '#       #',
-             '#########'], (-float(150), float(40))),
-}
+class HoleyGrid(Holes, Grid):
+  def __init__(self, level='Train', render_mode=None, **kwargs):
+    Holes.__init__(self, level, max_episode_steps=100, **kwargs)
+    Grid.__init__(self, render_mode=render_mode)
 
-class HoleyGrid(Grid):
-  def __init__(self, render_mode=None, level='Train', **kwargs):
-    layout, self.reward_range = LEVELS[level]; self._name = f'Holes{level}'
-    super(HoleyGrid, self).__init__((7,9), layout, render_mode, **kwargs)
+if __name__ == "__main__":  
+  import sys
+  from hyphi_gym import named, Monitor
+  import gymnasium as gym
+
+  demo = '--demo' in sys.argv[1:]; path = {'Holes': [2,1,1,1,1,1,0,1], 'HolesShift': [2,1,1]}
+  if demo: sys.argv.pop(sys.argv.index('--demo'))
+  envs = sys.argv[1:] if len(sys.argv[1:]) else ['Holes', 'HolesShift']
+  if '-h' in envs: print('Provide names of Maze environments to be rendered'); sys.exit(0)
+  for name in envs:
+    env = Monitor(gym.make(**named(name), render_mode='3D', seed=42), record_video=demo)
+    env.reset(); env.save_image(f'hyphi_gym/assets/render/{name}.png')
+    if demo: 
+      for a in path[name]: env.step(a)
+      env.save_video(f'hyphi_gym/assets/render/{name}.gif')
