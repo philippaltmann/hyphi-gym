b4573255e348d673cc9637c111625691322e276a
Philipp Altmann <philipp.altmann@ifi.lmu.de>
2023-07-21 08:53:05 +0200

    Updated GridMaze Demo Rendering

--- hyphi_gym/envs/GridMaze/grid_maze.py
+++ hyphi_gym/envs/GridMaze/grid_maze.py
@@ -7,18 +7,15 @@ class GridMaze(Maze,Grid):
 
 # Create Renderings
 if __name__ == "__main__":
-  import sys; from functools import reduce
+  import sys
   from hyphi_gym import named, Monitor
   import gymnasium as gym
-  args = sys.argv[1:] if len(sys.argv[1:]) else ['Maze7', 'Maze11', 'Maze13', 'Maze15'] 
+  envs = sys.argv[1:] if len(sys.argv[1:]) else ['Maze7', 'Maze11', 'Maze13', 'Maze15'] 
   #['Mazes7', 'Mazes11', 'Mazes13', 'Mazes15']
-  if '-h' in args[0]: print('Provide names of Maze environments to be rendered'); sys.exit(0)
-  mazes, maze = reduce(lambda x, y: x[not 'Mazes' in y].append(y) or x, args, ([], []))
-  for name in maze:
-    env = Monitor(gym.make(**named(name), render_mode='blender', seed=42))
-    env.reset(); env.save_image(f'hyphi_gym/assets/render/{name}.png')
-
-  for name in mazes:
+  if '-h' in envs[0]: print('Provide names of Maze environments to be rendered'); sys.exit(0)
+  for name in envs:
     env = Monitor(gym.make(**named(name), render_mode='blender', seed=42), record_video=True)
-    for _ in range(100): env.reset()
-    env.save_video(f'hyphi_gym/assets/render/{name}.gif')
+    if len(env.random): 
+      for _ in range(10): env.reset()
+      env.save_video(f'hyphi_gym/assets/render/{name}.gif')
+    else: env.reset(); env.save_image(f'hyphi_gym/assets/render/{name}.png')
