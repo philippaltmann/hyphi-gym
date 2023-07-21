0eff5ee818183bf3ccac91f97049517544aa7e2a
Philipp Altmann <philipp.altmann@ifi.lmu.de>
2023-07-21 08:52:42 +0200

    Updated Holey Grid Demo Rendering

--- hyphi_gym/envs/HoleyGrid/holey_grid.py
+++ hyphi_gym/envs/HoleyGrid/holey_grid.py
@@ -9,14 +9,22 @@ if __name__ == "__main__":
   import sys
   from hyphi_gym import named, Monitor
   import gymnasium as gym
-
-  demo = '--demo' in sys.argv[1:]; path = {'Holes': [2,1,1,1,1,1,0,1], 'HolesShift': [2,1,1]}
+  demo = '--demo' in sys.argv[1:]; 
+  path = {'HoleyGrid': [2,1,1,1,1,1,0,1], 'HoleyGrid': [2,1,1,1,1,1,0,1], 'HoleyGridShift': [2,1,1]} 
   if demo: sys.argv.pop(sys.argv.index('--demo'))
-  envs = sys.argv[1:] if len(sys.argv[1:]) else ['Holes', 'HolesShift']
+  envs = sys.argv[1:] if len(sys.argv[1:]) else ['HoleyGrid', 'HoleyGridShift']
   if '-h' in envs: print('Provide names of Maze environments to be rendered'); sys.exit(0)
   for name in envs:
-    env = Monitor(gym.make(**named(name), render_mode='3D', seed=42), record_video=demo)
-    env.reset(); env.save_image(f'hyphi_gym/assets/render/{name}.png')
+    env = Monitor(gym.make(**named(name), render_mode='blender', seed=42), record_video=True)
+    if len(env.random): 
+      for _ in range(10): env.reset()
+      env.save_video(f'hyphi_gym/assets/render/{name}.gif')
+    else: env.reset(); env.save_image(f'hyphi_gym/assets/render/{name}.png')
     if demo: 
-      for a in path[name]: env.step(a)
+      if len(env.random): 
+        for _ in range(10): 
+          env.reset()
+          for a in path[name]: env.step(a)
+      else: 
+        for a in path[name]: env.step(a)
       env.save_video(f'hyphi_gym/assets/render/{name}.gif')
