20ba98c28be65353b5d5722dd52dab6fc93ee18d
Philipp <philipp.altmann@ifi.lmu.de>
2023-07-25 19:11:43 +0100

    Fixed typing

--- hyphi_gym/envs/common/base.py
+++ hyphi_gym/envs/common/base.py
@@ -1,5 +1,5 @@
-from typing import Any, List, Optional, SupportsFloat
 import gymnasium as gym; 
+from typing import Optional, SupportsFloat
 from gymnasium.utils.seeding import np_random
 from gymnasium.envs.registration import EnvSpec
 
@@ -11,7 +11,7 @@ class Base(gym.Env):
   • Exploration Mode, where both the target and the reward are removed. 
   • Seeding nondeterministic environemtn configureation
   • Generating a dynamic spec obejct and env name based on the configuration"""
-  random: List; termination_resaons:List; _name: str
+  random:list; _name: str
 
   def __init__(self, max_episode_steps=100, sparse=False, explore=False, seed:Optional[int]=None):
     self.max_episode_steps, self.reward_range = max_episode_steps, (-max_episode_steps, -max_episode_steps)
@@ -35,26 +35,26 @@ class Base(gym.Env):
     return EnvSpec(**{**self._spec, **{k:getattr(self,k) for k in self.dynamic_spec}})
 
   @spec.setter
-  def spec(self, spec: dict[str,Any]): 
+  def spec(self, spec: dict): 
     """Function to mutate the internal environment spec (e.g., for adapting max_episode_steps)"""
     self._spec = {k:v for k,v in spec.__dict__.items() if k not in ['namespace','name','version']}
 
   def seed(self, seed:Optional[int]=None): self.np_random, self._seed = np_random(seed)
 
-  def reset(self, **kwargs)->tuple[gym.spaces.Space, dict[str, Any]]:
+  def reset(self, **kwargs)->tuple[gym.spaces.Space, dict]:
     """Gymnasium compliant function to reset the environment""" 
     self.reward_buffer, self.termination_resaons = [], []; 
     return super().reset(**kwargs)
 
-  def _step(self, action:gym.spaces.Space) -> tuple[Any, SupportsFloat, bool, bool, dict[str, Any]]: 
+  def _step(self, action:gym.spaces.Space) -> tuple[gym.spaces.Space,SupportsFloat, bool, bool, dict]: 
     """Overwrite this function to perfom step mutaions on the actual environment"""
     raise(NotImplementedError)
   
-  def step(self, action:gym.spaces.Space) -> tuple[gym.spaces.Space, SupportsFloat, bool, bool, dict[str, Any]]:
+  def step(self, action:gym.spaces.Space) -> tuple[gym.spaces.Space, SupportsFloat, bool, bool, dict]:
     """Gymnasium compliant fucntion to step the environment with `action` using the internal `_step`"""
     state, reward, terminated, truncated, info = self._step(action); self.reward_buffer.append(reward)
     if self.explore: reward = 0
     truncated = self.max_episode_steps is not None and len(self.reward_buffer) >= self.max_episode_steps
-    if truncated: info = {'termination_reason': 'TIME', **info}; self.termination_resaons.append('TIME')
+    if truncated: info = {'termination_reason': 'TIME', **info}
     if self.sparse: reward = 0 if not (terminated or truncated) else sum(self.reward_buffer)
     return state, reward, terminated, truncated, info
