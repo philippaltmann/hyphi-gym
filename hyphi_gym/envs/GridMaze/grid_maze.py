f061a4392d1969a18fa95d8c304b315f3c6585bb
Philipp <philipp.altmann@ifi.lmu.de>
2023-07-29 02:11:03 -1000

    Moved maze demos to test

--- hyphi_gym/envs/GridMaze/grid_maze.py
+++ hyphi_gym/envs/GridMaze/grid_maze.py
@@ -4,18 +4,3 @@ class GridMaze(Maze,Grid):
   def __init__(self, render_mode=None, **kwargs):
     Maze.__init__(self, max_episode_steps=100, **kwargs)
     Grid.__init__(self, render_mode=render_mode)
-
-# Create Renderings
-if __name__ == "__main__":
-  import sys
-  from hyphi_gym import named, Monitor
-  import gymnasium as gym
-  envs = sys.argv[1:] if len(sys.argv[1:]) else ['Maze7', 'Maze11', 'Maze13', 'Maze15'] 
-  #['Mazes7', 'Mazes11', 'Mazes13', 'Mazes15']
-  if '-h' in envs[0]: print('Provide names of Maze environments to be rendered'); sys.exit(0)
-  for name in envs:
-    env = Monitor(gym.make(**named(name), render_mode='blender', seed=42), record_video=True)
-    if len(env.random): 
-      for _ in range(10): env.reset()
-      env.save_video(f'hyphi_gym/assets/render/{name}.gif')
-    else: env.reset(); env.save_image(f'hyphi_gym/assets/render/{name}.png')
--- hyphi_gym/envs/PointMaze/point_maze.py
+++ hyphi_gym/envs/PointMaze/point_maze.py
@@ -4,17 +4,3 @@ class PointMaze(Point, Maze): # Point(Mujoco) | Maze(Board(Base))
   def __init__(self, render_mode=None, **kwargs):
     Maze.__init__(self, max_episode_steps=800, **kwargs) # Init Maze
     Point.__init__(self, render_mode=render_mode)
-
-
-if __name__ == "__main__":
-  from hyphi_gym import named, Monitor
-  import gymnasium as gym
-  render = True; name = "PointMazes15"
-  env = Monitor(gym.make(**named(name), seed=42, render_mode='3D'), record_video=render)
-  for i in range(4):
-    print(i); obs, _ = env.reset(); R = []
-    for a in [env.action_space.sample() for _ in range(100)]: 
-      observation, reward, terminated, truncated, info = env.step(a); R.append(reward)
-      if terminated or truncated: break
-  if render: env.save_video(f'test/video/{name}.mp4')
-
