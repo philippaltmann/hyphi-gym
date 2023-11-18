97f7910a9497ef346e30f6bcd0624231b83b2e57
Philipp <philipp.altmann@ifi.lmu.de>
2023-11-18 18:27:46 +0100

    Updated test script

--- test/__main__.py
+++ test/__main__.py
@@ -1,9 +1,6 @@
-import argparse
+import argparse; import numpy as np; import math; from PIL import Image
+import gymnasium as gym; from gymnasium.wrappers.autoreset import AutoResetWrapper
 from hyphi_gym import named, Monitor
-import gymnasium as gym
-from PIL import Image
-import numpy as np; import math
-from gymnasium.wrappers.autoreset import AutoResetWrapper
 
 parser = argparse.ArgumentParser()
 parser.add_argument('envs', nargs='+', help='A list of environments to test')
@@ -11,7 +8,6 @@ parser.add_argument('--demo', nargs='+', default=[], type=int, help='A list of a
 parser.add_argument('--runs', type=int, default=9, help='Number of random configurations to be generated')
 parser.add_argument('--square', action='store_true', help='Save suare images instead of 3:2')
 parser.add_argument('--grid', action='store_true', help='Save grid of layouts instead of gif')
-
 args = parser.parse_args()
 
 def image_grid(imgs, rows, cols):
@@ -21,18 +17,29 @@ def image_grid(imgs, rows, cols):
     grid.paste(img, box=(i%cols*w, i//cols*h))
   return grid
 
+disc = lambda d, lo=-1, hi=1: [[*([0]*n), v, *([0]*(d-n-1))] for v in (hi,lo) for n in range(d)[::-1]]
+
 for name in args.envs:
   if "Point" in name or "Plane" in name:
-    render,lookup,n = '2D', [[0,1],[1,0],[0,-1],[-1,0]], 20
+    render,lookup,n = '3D', disc(2), 20
     demo = [lookup[a] for a in args.demo for _ in range(n)]
-  else: demo, render = args.demo, 'blender'
+    # args.square = True
+
+  elif "Fetch" in name:
+    # 0       1  2     3     4       5    6    7
+    # Gripper Up Right Front Gripper Down Left Back
+    render, lookup, n = '3D', disc(4), 1
+    args.square = True
+    demo = [lookup[a] for a in args.demo for _ in range(n)]
+  else: demo, render = args.demo, '3D'#'blender'
 
   env = AutoResetWrapper(Monitor(
       gym.make(**named(name), render_mode=render, seed=42)
     , record_video=True)) #len(args.demo)>0)
-  layouts = []
+  layouts = []; env.reset(); tm, tr = False, False
   for i in range(args.runs if len(env.unwrapped.random) else 1): 
-    env.reset(seed=i); render = env.render(); 
+    if not (tm or tr): env.reset(seed=i)
+    render = env.render(); 
     if isinstance(render, np.ndarray): render = Image.fromarray(render)
     if not args.square: w = 720; h = w/3*2; c = (w-h)/2; render = render.crop((0,c,w,h+c))
     layouts.append(render) 
@@ -47,7 +54,7 @@ for name in args.envs:
     layouts[0].save(
       f"test/render/{name}.{'gif' if len(layouts) > 1 else 'png'}",
       save_all=True, append_images=layouts[1:], optimize=False, 
-      duration=1000/env.metadata['render_fps'], loop=0
+      loop=0, duration=200 #100/env.metadata['render_fps']
     )
   # if len(layouts) > 1: image_grid(layouts, 10, 10).save(f"test/render/{name}-grid.png")
 
