5e6206d05be27a8e541f7201a39dfd5f69cc5007
Philipp <philipp.altmann@ifi.lmu.de>
2023-12-15 10:59:16 +0100

    Added FlatGrid env

--- hyphi_gym/envs/__init__.py
+++ hyphi_gym/envs/__init__.py
@@ -1,4 +1,5 @@
 from hyphi_gym.envs.Fetch import Fetch
+from hyphi_gym.envs.FlatGrid import FlatGrid
 from hyphi_gym.envs.GridMaze import GridMaze
 from hyphi_gym.envs.HoleyGrid import HoleyGrid
 from hyphi_gym.envs.HoleyPlane import HoleyPlane
