a6066a0d447cfd217edd9eaafeb23f11d1a8f30b
Philipp <philipp.altmann@ifi.lmu.de>
2023-12-15 10:58:34 +0100

    Added FlatGrid env

---
+++ hyphi_gym/envs/FlatGrid/__init__.py
@@ -0,0 +1,8 @@
+from hyphi_gym.common import Grid
+from hyphi_gym.common.board import Board
+
+class FlatGrid(Grid, Board):
+  def __init__(self, size, random=[], render_mode=None, **kwargs):
+    self._name = f'FlatGrid{size}' 
+    Board.__init__(self, size=(size,size), layout=None, random=random, max_episode_steps=100, **kwargs)
+    Grid.__init__(self, render_mode=render_mode)
